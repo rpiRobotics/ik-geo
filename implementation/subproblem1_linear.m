7adf094584fd564eca07c2a5e1d58ec0c41c4e05
Alex Elias <aelias36@yahoo.com>
2022-08-26 20:13:01 -0400

    Update subproblem1_linear.m

--- implementation/subproblem1_linear.m
+++ implementation/subproblem1_linear.m
@@ -2,11 +2,9 @@ function theta = subproblem1_linear(p1, p2, k)
 %  p2 = rot(k, theta)*p1
 
 KxP = cross(k, p1);
-
 A = [KxP -cross(k,KxP)];
-p = p2-k*k'*p1;
 
-x = A'*p;
+x = A'*p2;
 
 theta = atan2(x(1),x(2));
 
