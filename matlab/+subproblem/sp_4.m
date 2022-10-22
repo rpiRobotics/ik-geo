a63be3ad553a44dc4da0b2e5ea4baac0f8eace2f
aelias36 <aelias36@yahoo.com>
2022-10-22 14:59:48 -0400

    Update sp_4.m

--- matlab/+subproblem/sp_4.m
+++ matlab/+subproblem/sp_4.m
@@ -34,23 +34,18 @@ norm_A_2 = dot(A,A);
 
 x_ls = A_1'*(h*b);
 
-if norm(norm_A_2) > b^2
+if norm_A_2 > b^2
     xi = sqrt(norm_A_2-b^2);
     A_perp_tilde = [A(2); -A(1)];
 
-    
     sc_1 = x_ls + xi*A_perp_tilde;
     sc_2 = x_ls - xi*A_perp_tilde;
 
     theta = [atan2(sc_1(1), sc_1(2)) atan2(sc_2(1), sc_2(2))];
-    if nargout > 1
-        is_LS = false;
-    end
+    is_LS = false;
 else
     theta = atan2(x_ls(1), x_ls(2));
-    if nargout > 1
-        is_LS = true;
-    end
+    is_LS = true;
 end
 
 end
