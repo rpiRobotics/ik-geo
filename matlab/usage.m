6d3b748319df00822dc69d7c01968c45c4a62981
Alex Elias <aelias36@yahoo.com>
2022-10-23 20:13:48 -0400

    Update usage.m

--- matlab/usage.m
+++ matlab/usage.m
@@ -1,6 +1,4 @@
-addpath("implementation\")
-
-% Also make sure to add path to general-robotics-toolbox
+% Make sure to add path to general-robotics-toolbox
 
 %% Subproblem 1
 
@@ -28,36 +26,19 @@ subproblem_setups.sp_3.error(P, S_t)
 %% Subproblem 4
 
 [P, S] = subproblem_setups.sp_4.setup()
-
 S_t.theta = subproblem.sp_4(P.h,P.p,P.k,P.d)
-
 subproblem_setups.sp_4.error(P, S_t)
 
 %% Subproblem 5
 
 [P, S] = subproblem_setups.sp_5.setup()
-
-% global H_ap
-% global N_ap
-% global x1_ap
-% H_ap = k2'*(p0 + rot(k1,theta1)*p1)
-% k2'*(p2+rot(k3,theta3)*p3)
-% 
-% N_ap = norm(p0 + rot(k1,theta1)*p1 - H_ap*k2)
-% norm(p2+rot(k3,theta3)*p3 - H_ap*k2)
-% 
-% x1_ap = [sin(theta1); cos(theta1)]
-
 [S_t.theta1, S_t.theta2, S_t.theta3] = ...
     subproblem.sp_5(P.p0,P.p1,P.p2,P.p3,P.k1,P.k2,P.k3);
-
 subproblem_setups.sp_5.error(P,S_t)
 
 %% Subproblem 6
 
 [P, S] = subproblem_setups.sp_6.setup()
-
 [S_t.theta1, S_t.theta2] = subproblem.sp_6( ...
-P.H, P.K, P.P, P.d1, P.d2)
-
+    P.H, P.K, P.P, P.d1, P.d2)
 subproblem_setups.sp_6.error(P,S_t)
