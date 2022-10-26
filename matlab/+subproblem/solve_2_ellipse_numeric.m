d685705cadc0ec96d65f1fcb21a7df6be2c0f9b7
Alex Elias <aelias36@yahoo.com>
2022-10-26 17:29:33 -0400

    Update usage.m, delete old files

--- matlab/+subproblem/solve_2_ellipse_numeric.m
+++ matlab/+subproblem/solve_2_ellipse_numeric.m
@@ -1,6 +1,7 @@
 function [xi_1, xi_2] = solve_2_ellipse_numeric(xm1, xn1, xm2, xn2)
-% solve for intersection of 2 ellipses defined by
-
+% subproblem.solve_2_ellipse_numeric Numeric Ellipse Intersection Solution
+% Solve for intersection of 2 ellipses defined by
+%
 % xm1'*xm1 + xi'*xn1'*xn1*xi  + xm1'*xn1*xi == 1
 % xm2'*xm2 + xi'*xn2'*xn2*xi  + xm2'*xn2*xi == 1
 % Where xi = [xi_1; xi_2]
