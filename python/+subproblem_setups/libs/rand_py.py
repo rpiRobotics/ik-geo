3f5bc330df1dfc88a1bbcaf78add9cadd14fef27
AshtonRopp <ashton.ropp@gmail.com>
2022-10-31 22:21:12 -0400

    Added in total functionality for sp1.  Will move to sp2 after tomorrow's meeting

--- python/+subproblem_setups/libs/rand_py.py
+++ python/+subproblem_setups/libs/rand_py.py
@@ -2,24 +2,24 @@ import numpy as np #Used for matrix operations
 import math        #Used for pi
 import random as r
 
-
-#Standard 3 x 3 eigen vector
-eye = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-
 def hat(k):
-   return np.array([[0, -k[2][0], k[1][0]], [k[2][0], 0, -k[0][0]], [-k[1][0], k[0][0], 0]])
+   return np.array([[0, -k[2], k[1]], [k[2], 0, -k[0]], [-k[1], k[0], 0]])
 
 #Create a random angle
 def rand_angle():
    return r.random()*2*math.pi-math.pi
 
 #Create random matrix of size 3 x num (rows, cols)
-def rand_vec(size = 1): #Make list of lists, then pass to numpy
+def rand_mat(size = 1): #Make list of lists, then pass to numpy
    return np.array([[r.random() for x in range(0,size)] for y in range(0, 3)])
+
+   #Create random matrix of size 3 x num (rows, cols)
+def rand_vec(): #Make list of lists, then pass to numpy
+   return np.array([r.random() for y in range(0, 3)])
 
 #Create random normal vector
 def rand_normal_vec(size = 1):
-   vec = rand_vec(size)
+   vec = rand_vec()
    return vec/np.linalg.norm(vec, 2)
 
 #Create rand perp vec from input numpi.array
@@ -30,4 +30,4 @@ def rand_perp_normal_vec(inp):
 
 def rot(k, theta):
    k = k/np.linalg.norm(k,2)
-   return eye + math.sin(theta)*hat(k)+(1-math.cos(theta))*hat(k)@hat(k)
+   return np.eye(3) + math.sin(theta)*hat(k)+(1-math.cos(theta))*hat(k)@hat(k)
--- python/+subproblem_setups/sp_1.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import numpy as np
- 
-
-
-#Set path and then import
-sys.path.append('../')
-import rand_py as r
