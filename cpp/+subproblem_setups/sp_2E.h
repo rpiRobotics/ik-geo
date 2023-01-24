f86e4193624529e96a1b328ad12457bbfd1223a6
OTLorz2 <56813177+OTLorz2@users.noreply.github.com>
2023-01-24 15:44:10 -0500

    Updated sp_2E.cpp and sp_2E.h

--- cpp/+subproblem_setups/sp_2E.h
+++ cpp/+subproblem_setups/sp_2E.h
@@ -14,8 +14,9 @@
 using namespace Eigen;
 
 void sp_2E_setup(Vector3d &p0, Vector3d &p1, Vector3d &p2, Vector3d &k1, Vector3d &k2, 
-		  					 double &theta1, double &theta2);
-void sp_2E(Vector3d &p0, Vector3d &p1, Vector3d &p2, Vector3d &k1, Vector3d &k2, 
-		  		 double &theta1, double &theta2);
+		  double &theta1, double &theta2);
+
+void sp_2E(const Vector3d &p0, const Vector3d &p1, const Vector3d &p2, const Vector3d &k1, const Vector3d &k2, 
+		  double &theta1, double &theta2);
 
 #endif
