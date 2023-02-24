c44cbe086084f406181ffd99bcadff25051127d7
Joyal Mathew <joyal.mathew2023@gmail.com>
2023-02-23 20:09:40 -0500

    3 parallel

--- rust/src/inverse_kinematics/mod.rs
+++ rust/src/inverse_kinematics/mod.rs
@@ -1,5 +1,7 @@
 use std::f64::consts::{PI, TAU};
 
+use crate::subproblems::subproblem6;
+
 pub mod auxiliary;
 pub mod setups;
 pub mod hardcoded;
@@ -200,7 +202,7 @@ pub fn three_parallel_two_intersecting(r_06: &Matrix3<f64>, p_0t: &Vector3<f64>,
         );
 
         for q5 in theta5.get_all() {
-            let (theta_14, theta_14_is_ls) = subproblem1(
+            let (theta14, theta_14_is_ls) = subproblem1(
                 &(rot(&kin.h.column(4).into(), q5) * kin.h.column(5)),
                 &(r_01.transpose() * r_06 * kin.h.column(5)),
                 &kin.h.column(1).into()
@@ -208,7 +210,7 @@ pub fn three_parallel_two_intersecting(r_06: &Matrix3<f64>, p_0t: &Vector3<f64>,
 
             let r_01 = rot(&kin.h.column(0).into(), q1);
             let r_45 = rot(&kin.h.column(4).into(), q5);
-            let r_14 = rot(&kin.h.column(1).into(), theta_14);
+            let r_14 = rot(&kin.h.column(1).into(), theta14);
 
             let p_12: &Vector3<f64> = &kin.p.column(1).into();
             let p_23: &Vector3<f64> = &kin.p.column(2).into();
@@ -223,7 +225,7 @@ pub fn three_parallel_two_intersecting(r_06: &Matrix3<f64>, p_0t: &Vector3<f64>,
             for q3 in theta_3.get_all() {
                 let (q2, q2_is_ls) = subproblem1(&(p_23 + rot(&kin.h.column(1).into(), q3) * p_34), &d_inner, &kin.h.column(1).into());
 
-                let q4 = (theta_14 - q2 - q3 + PI) % TAU - PI;
+                let q4 = (theta14 - q2 - q3 + PI) % TAU - PI;
 
                 let (q6, q6_is_ls) = subproblem1(
                     &kin.h.column(4).into(),
@@ -234,6 +236,49 @@ pub fn three_parallel_two_intersecting(r_06: &Matrix3<f64>, p_0t: &Vector3<f64>,
                 q.push(Vector6::new(q1, q2, q3, q4, q5, q6));
                 is_ls.push(theta1_is_ls || theta5_is_ls || theta_14_is_ls || theta_3_is_ls || q2_is_ls || q6_is_ls);
             }
+        }
+    }
+
+    (q, is_ls)
+}
+
+pub fn three_parallel(r_06: &Matrix3<f64>, p_0t: &Vector3<f64>, kin: &Kinematics) -> (Vec<Vector6<f64>>, Vec<bool>) {
+    let mut q = Vec::with_capacity(6);
+    let mut is_ls = Vec::with_capacity(6);
+
+    let p_16 = p_0t - kin.p.column(0) - r_06 * kin.p.column(6);
+
+    let h_sp: [Vector3<f64>; 4] = [kin.h.column(1).into(), kin.h.column(1).into(), kin.h.column(1).into(), kin.h.column(1).into()];
+    let k_sp: [Vector3<f64>; 4] = [-kin.h.column(0), kin.h.column(4).into(), -kin.h.column(0), kin.h.column(4).into()];
+    let p_sp: [Vector3<f64>; 4] = [p_16, -kin.p.column(5), r_06 * kin.h.column(5), -kin.h.column(5)];
+    let d1 = (kin.h.column(1).transpose() * (kin.p.column(2) + kin.p.column(1) + kin.p.column(4) + kin.p.column(1)))[0];
+    let d2 = 0.0;
+
+    let theta15 = subproblem6(&h_sp, &k_sp, &p_sp, d1, d2);
+
+    for (q1, q5) in theta15.get_all() {
+        let (theta14, theta14_is_ls) = subproblem1(&(rot(&kin.h.column(4).into(), q5) * kin.h.column(5)), &(rot(&kin.h.column(0).into(), -q1) * r_06 * kin.h.column(5)), &kin.h.column(1).into());
+
+        let r_01 = rot(&kin.h.column(0).into(), q1);
+        let r_45 = rot(&kin.h.column(4).into(), q5);
+        let r_14 = rot(&kin.h.column(1).into(), theta14);
+        let p_12 = kin.p.column(1);
+        let p_23 = kin.p.column(2);
+        let p_34 = kin.p.column(3);
+        let p_45 = kin.p.column(4);
+        let p_56 = kin.p.column(5);
+        let d_inner = r_01.transpose() * p_16 - p_12 - r_14 * r_45 * p_56 - r_14 * p_45;
+        let d = d_inner.norm();
+        let (theta3, theta3_is_ls) = subproblem3(&-p_34, &p_23.into(), &kin.h.column(1).into(), d);
+
+        for q3 in theta3.get_all() {
+            let (q2, q2_is_ls) = subproblem1(&(p_23 + rot(&kin.h.column(1).into(), q3) * p_34), &d_inner, &kin.h.column(1).into());
+            let q4 = (theta14 - q2 - q3 + PI) % TAU - PI;
+
+            let (q6, q6_is_ls) = subproblem1(&kin.h.column(4).into(), &(r_45.transpose() * r_14.transpose() * r_01.transpose() * r_06 * kin.h.column(4)), &kin.h.column(5).into());
+
+            q.push(Vector6::new(q1, q2, q3, q4, q5, q6));
+            is_ls.push(theta14_is_ls || theta3_is_ls || q2_is_ls || q6_is_ls);
         }
     }
 
