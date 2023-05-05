b3515392cece6bf222b81eb2bde349dcb7ff57a4
Runbin Chen <56813177+OTLorz2@users.noreply.github.com>
2023-05-05 16:48:53 -0400

    fixed formatting

--- cpp/+subproblem_setups/sp_5.h
+++ cpp/+subproblem_setups/sp_5.h
@@ -10,8 +10,10 @@
 #include <vector>
 #include <eigen3/Eigen/Dense>
 
-void sp_5(const Eigen::Vector3d &p0, const Eigen::Vector3d &p1, const Eigen::Vector3d &p2, const Eigen::Vector3d &p3, 
-		  		const Eigen::Vector3d &k1, const Eigen::Vector3d &k2, const Eigen::Vector3d &k3, 
-		  		std::vector<double> &theta1, std::vector<double> &theta2, std::vector<double> &theta3);
+void sp5_setup(Eigen::Vector3d &p0, Eigen::Vector3d &p1, Eigen::Vector3d &p2, Eigen::Vector3d &p3, Eigen::Vector3d &k1, Eigen::Vector3d &k2, Eigen::Vector3d &k3);
+
+void sp5_run(const Eigen::Vector3d &p0, const Eigen::Vector3d &p1, const Eigen::Vector3d &p2, const Eigen::Vector3d &p3, 
+		  const Eigen::Vector3d &k1, const Eigen::Vector3d &k2, const Eigen::Vector3d &k3, 
+		  std::vector<double> &theta1, std::vector<double> &theta2, std::vector<double> &theta3);
 
 #endif
