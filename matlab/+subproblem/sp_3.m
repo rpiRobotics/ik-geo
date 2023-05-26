8d2eeb6edf1bb45d334d02d3e323ea7033a271a7
Alex Elias <aelias36@yahoo.com>
2023-05-25 20:21:44 -0400

    Update SP3: Solve using SP4

--- matlab/+subproblem/sp_3.m
+++ matlab/+subproblem/sp_3.m
@@ -24,36 +24,5 @@ function [theta, is_LS] = sp_3(p1, p2, k, d)
 %           (N is the number of solutions)
 %       is_LS: 1x1 boolean
 
-
-KxP = cross(k,p1);
-
-A_1 = [KxP -cross(k,KxP)];
-A = -2*p2'*A_1;
-norm_A_sq = dot(A,A);
-norm_A = sqrt(norm_A_sq);
-
-b = d^2 - norm(p2-k*k'*p1)^2 - norm(KxP)^2;
-
-x_ls = A_1'*(-2*p2*b/norm_A_sq);
-
-if dot(x_ls,x_ls)>1
-    theta = atan2(x_ls(1), x_ls(2));
-    if nargout > 1
-        is_LS = true;
-    end
-    return
-end
-
-xi = sqrt(1-b^2/norm_A_sq);
-
-A_perp_tilde = [A(2); -A(1)];
-A_perp = A_perp_tilde/ norm_A;
-
-sc_1 = x_ls + xi*A_perp;
-sc_2 = x_ls - xi*A_perp;
-
-theta = [atan2(sc_1(1), sc_1(2)) atan2(sc_2(1), sc_2(2))];
-if nargout > 1
-    is_LS = false;
-end
+[theta, is_LS] = subproblem.sp_4(p2, p1, k, 1/2 * (dot(p1,p1)+dot(p2,p2)-d^2));
 end
