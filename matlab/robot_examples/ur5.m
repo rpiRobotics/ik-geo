f2c26ade0571c9939427c7c61d2dd76aac09b81f
Alex Elias <aelias36@yahoo.com>
2024-09-26 13:41:10 -0400

    Update ur5.m

--- matlab/robot_examples/ur5.m
+++ matlab/robot_examples/ur5.m
@@ -15,7 +15,7 @@ ey = [0;1;0];
 ez = [0;0;1];
 
 P.kin.H = [ez ey ey ey -ez ey];
-P.kin.P = [0.089159*ez, 0.1358*ey, -0.1197*ey+0.425*ex, 0.3922*ex, 0.093*ey, -0.0946*ez, 0.0823*ey];
+P.kin.P = [0.089159*ez, 0.1358*ey, -0.1197*ey+0.425*ex, 0.3922*ex, 0.093*ey-0.0946*ez, zv, 0.0823*ey];
 P.kin.joint_type = zeros([6 1]);
 
 % Pick a joint configuration find the associated end effector pose
@@ -27,4 +27,4 @@ q_true = rand_angle([6 1]);
 [S.Q, S.is_LS] = IK.IK_3_parallel_2_intersecting(P.R, P.T, P.kin);
 S.Q
 S.is_LS
-IK_setups.IK_3_parallel_2_intersecting.error(P,S)
+[e, e_R, e_T] = IK_setups.IK_3_parallel_2_intersecting.error(P,S)
