ee7a973a5f0bf7efe0b94b9520b20b5780bf592a
Alex Elias <aelias36@yahoo.com>
2024-12-20 22:25:50 -0500

    Update test_MM50_SJ2.cpp
    Refactor

--- cpp/tests_and_demos/test_MM50_SJ2.cpp
+++ cpp/tests_and_demos/test_MM50_SJ2.cpp
@@ -8,7 +8,19 @@ To compile:
 g++ -Wall -O3 tests_and_demos/test_MM50_SJ2.cpp hardcoded_SEW_IK/Motoman_50_SJ2.cpp SEW_IK/IK_R_2R_R_3R_SJ2.cpp subproblems/sp.cpp utils.cpp -I. -std=c++17 -o test_MM50_SJ2
 */
 
+void test_MM50_random();
+void test_MM50_csv();
+
 int main() {
+    test_MM50_random();
+    std::cout << "----------------------------------------" << std::endl;
+    test_MM50_csv();
+
+    return 0;
+}
+
+
+void test_MM50_random() {
     // Create an instance of the Motoman_50_SJ2_Setup class
     Motoman_50_SJ2_Setup setup;
 
@@ -30,31 +42,28 @@ int main() {
     std::cout << "Error to q_given: " << error_to_q_given << std::endl;
 
     setup.debug();
+}
 
-    std::cout << "----------------------------------------" << std::endl;
-
-    // Now test importing a CSV line
+void test_MM50_csv() {
     std::string csv_line = "-0.057996647518894,0.000440446470090519,0.998316680659737,-0.438620746249246,0.898299606595998,-0.0258777462161887,-0.89679887925684,-0.439383229989928,-0.0519051766907153,0.115426042890603,0.0931067009559481,-0.130789826252996,2.79249835640583,0.294565272647012,2.87460022633501,2.92098081384033,-2.15128045457413,2.95682165202307,2.87246465212821,-0.0918875090716771";
-    Motoman_50_SJ2_Setup setup2(csv_line);
+    Motoman_50_SJ2_Setup setup(csv_line);
 
     // Run the inverse kinematics solver
-    setup2.run();
+    setup.run();
 
     // Print the solutions
     std::cout << "Solutions:" << std::endl;
-    for (size_t i = 0; i < setup2.sol.q.size(); ++i) {
-        std::cout << "Solution " << i + 1 << ": " << setup2.sol.q[i].transpose() << std::endl;
+    for (size_t i = 0; i < setup.sol.q.size(); ++i) {
+        std::cout << "Solution " << i + 1 << ": " << setup.sol.q[i].transpose() << std::endl;
     }
 
     // Calculate and print the error
-    double error2 = setup2.error();
+    double error2 = setup.error();
     std::cout << "Error: " << error2 << std::endl;
 
     // Calculate and print the error to the given q
-    double error_to_q_given2 = setup2.error_to_q_given();
+    double error_to_q_given2 = setup.error_to_q_given();
     std::cout << "Error to q_given: " << error_to_q_given2 << std::endl;
 
-    setup2.debug();
-
-    return 0;
+    setup.debug();
 }
