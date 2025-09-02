c93169fc5240002619fe6da647cdc6a00da7b546
Alex Elias <aelias36@yahoo.com>
2025-09-02 00:20:54 -0400

    Update unique_q_tol.m
    new infinity norm option

--- matlab/robot_IK_helpers/unique_q_tol.m
+++ matlab/robot_IK_helpers/unique_q_tol.m
@@ -1,21 +1,31 @@
-function Q_unique = unique_q_tol(Q, tol)
-    if nargin < 2
-        tol = 1e-6;
+function Q_unique = unique_q_tol(Q, tol, mode)
+    arguments
+        Q (:,:) double
+        tol (1,1) double = 1e-6
+        mode (1,1) string {mustBeMember(mode, ["euclidean", "infinity"])} = "euclidean"
     end
-    
+
     if isempty(Q)
         Q_unique = Q;
         return;
     end
-    
+
     Q_unique = Q(:,1); % Start with the first column
-    
+
     for i = 2:size(Q, 2)
         diffs = wrapToPi(Q_unique - Q(:,i));
-        dists = vecnorm(diffs, 2, 1);
-        
-        if all(dists > tol)
-            Q_unique = [Q_unique, Q(:,i)]; % Append if it's unique enough
+
+        switch mode
+            case "euclidean"
+                dists = vecnorm(diffs, 2, 1);
+                is_unique = all(dists > tol);
+            case "infinity"
+                max_diffs = max(abs(diffs), [], 1);
+                is_unique = all(max_diffs > tol);
+        end
+
+        if is_unique
+            Q_unique = [Q_unique, Q(:,i)];
         end
     end
 end
